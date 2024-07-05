432055a5fce4180b06e2a289f8b239fbc94158e0
Ed <Edgrant209@googlemail.com>
2024-07-05 19:35:59 -0400

    Added config details

--- src/main/java/com/entity_logger/EntityLoggerConfig.java
+++ src/main/java/com/entity_logger/EntityLoggerConfig.java
@@ -1,7 +1,5 @@
 package com.entity_logger;
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.*;
 
 import java.awt.Color;
 import java.util.Locale;
@@ -9,21 +7,321 @@ import java.util.Locale;
 @ConfigGroup("example")
 public interface EntityLoggerConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+
+	////////////////////////////////////////////////////////////////
+	//////////////////////////////// Entity Highlight Settings
+	////////////////////////////////////////////////////////////////
+
+	@ConfigSection(
+			name = "Entity highlight settings",
+			description = "Settings that control entity highlighting",
+			position = 0,
+			closedByDefault = true
 	)
-	default String greeting()
+	String entityHighlightSettings = "Entity Highlight Settings";
+
+	@ConfigItem(
+			keyName = "toggleHighlightInscopeHull",
+			name = "Highlight in-scope entities by hull",
+			description = "Toggle whether in-scope entities will be highlighted by hull",
+			section = entityHighlightSettings,
+			position = 0
+	)
+	default boolean toggleHighlightInscopeHull()
 	{
-		return "Hello";
-	} //TODO: Get rid of this
+		return false;
+	}
+	@ConfigItem(
+			keyName = "highlightInscopeHullColor",
+			name = "Color to highlight in-scope entities by hull",
+			description = "Which color to highlight in-scope entities by hull",
+			section = entityHighlightSettings,
+			position = 1
+	)
+	default Color highlightInscopeHullColor()
+	{
+		return new Color(0xFFFFFF00);
+	}
+	@ConfigItem(
+			keyName = "highlightInscopeHullInteriorColor",
+			name = "Color to highlight in-scope entities by hull interior",
+			description = "Which color to highlight in-scope entities by hull interior",
+			section = entityHighlightSettings,
+			position = 2
+	)
+	default Color highlightInscopeHullInteriorColor()
+	{
+		return new Color(0x1FFFFF00);
+	}
+	@ConfigItem(
+			keyName = "toggleHighlightInscopeOutline",
+			name = "Highlight in-scope entities by outline",
+			description = "Toggle whether in-scope entities will be highlighted by outline",
+			section = entityHighlightSettings,
+			position = 3
+	)
+	default boolean toggleHighlightInscopeOutline()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "highlightInscopeOutlineColor",
+			name = "Color to highlight in-scope entities by outline",
+			description = "Which color to highlight in-scope entities by outline",
+			section = entityHighlightSettings,
+			position = 4
+	)
+	default Color highlightInscopeOutlineColor()
+	{
+		return new Color(0xFFFFFF00);
+	}
+	@ConfigItem(
+			keyName = "highlightInscopeOutlineInteriorColor",
+			name = "Color to highlight in-scope entities by outline interior",
+			description = "Which color to highlight in-scope entities by outline interior",
+			section = entityHighlightSettings,
+			position = 5
+	)
+	default Color highlightInscopeOutlineInteriorColor()
+	{
+		return new Color(0x1FFFFF00);
+	}
+
+	@ConfigItem(
+			keyName = "toggleHighlightBackloggedHull",
+			name = "Highlight backlogged entities by hull",
+			description = "Toggle whether backlogged entities will be highlighted by hull",
+			section = entityHighlightSettings,
+			position = 6
+	)
+	default boolean toggleHighlightBackloggedHull()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "highlightBackloggedHullColor",
+			name = "Color to highlight backlogged entities by hull",
+			description = "Which color to highlight backlogged entities by hull",
+			section = entityHighlightSettings,
+			position = 7
+	)
+	default Color highlightBackloggedHullColor()
+	{
+		return new Color(0xFFFF0000);
+	}
+	@ConfigItem(
+			keyName = "highlightBackloggedHullInteriorColor",
+			name = "Color to highlight backlogged entities by hull interior",
+			description = "Which color to highlight backlogged entities by hull interior",
+			section = entityHighlightSettings,
+			position = 8
+	)
+	default Color highlightBackloggedHullInteriorColor()
+	{
+		return new Color(0x1FFF0000);
+	}
+	@ConfigItem(
+			keyName = "toggleHighlightBackloggedOutline",
+			name = "Highlight backlogged entities by outline",
+			description = "Toggle whether backlogged entities will be highlighted by outline",
+			section = entityHighlightSettings,
+			position = 9
+	)
+	default boolean toggleHighlightBackloggedOutline()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "highlightBackloggedOutlineColor",
+			name = "Color to highlight backlogged entities by outline",
+			description = "Which color to highlight backlogged entities by outline",
+			section = entityHighlightSettings,
+			position = 10
+	)
+	default Color highlightBackloggedOutlineColor()
+	{
+		return new Color(0xFFFF0000);
+	}
+	@ConfigItem(
+			keyName = "highlightBackloggedOutlineInteriorColor",
+			name = "Color to highlight backlogged entities by outline interior",
+			description = "Which color to highlight backlogged entities by outline interior",
+			section = entityHighlightSettings,
+			position = 11
+	)
+	default Color highlightBackloggedOutlineInteriorColor()
+	{
+		return new Color(0x1FFF0000);
+	}
+
+	@ConfigItem(
+			keyName = "toggleHighlightOutofscopeHull",
+			name = "Highlight out-of-scope entities by hull",
+			description = "Toggle whether out-of-scope entities will be highlighted by hull",
+			section = entityHighlightSettings,
+			position = 12
+	)
+	default boolean toggleHighlightOutofscopeHull()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "highlightOutofscopeHullColor",
+			name = "Color to highlight out-of-scope entities by hull",
+			description = "Which color to highlight out-of-scope entities by hull",
+			section = entityHighlightSettings,
+			position = 13
+	)
+	default Color highlightOutofscopeHullColor()
+	{
+		return new Color(0xFFFF00FF);
+	}
+	@ConfigItem(
+			keyName = "highlightOutofscopeHullInteriorColor",
+			name = "Color to highlight out-of-scope entities by hull interior",
+			description = "Which color to highlight out-of-scope entities by hull interior",
+			section = entityHighlightSettings,
+			position = 14
+	)
+	default Color highlightOutofscopeHullInteriorColor()
+	{
+		return new Color(0x1FFF00FF);
+	}
+	@ConfigItem(
+			keyName = "toggleHighlightOutofscopeOutline",
+			name = "Highlight out-of-scope entities by outline",
+			description = "Toggle whether out-of-scope entities will be highlighted by outline",
+			section = entityHighlightSettings,
+			position = 15
+	)
+	default boolean toggleHighlightOutofscopeOutline()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "highlightOutofscopeOutlineColor",
+			name = "Color to highlight out-of-scope entities by outline",
+			description = "Which color to highlight out-of-scope entities by outline",
+			section = entityHighlightSettings,
+			position = 16
+	)
+	default Color highlightOutofscopeOutlineColor()
+	{
+		return new Color(0xFFFF00FF);
+	}
+	@ConfigItem(
+			keyName = "highlightOutofscopeOutlineInteriorColor",
+			name = "Color to highlight out-of-scope entities by outline interior",
+			description = "Which color to highlight out-of-scope entities by outline interior",
+			section = entityHighlightSettings,
+			position = 17
+	)
+	default Color highlightOutofscopeOutlineInteriorColor()
+	{
+		return new Color(0x1FFF00FF);
+	}
+
+	@ConfigItem(
+			keyName = "toggleHighlightFinishedHull",
+			name = "Highlight finished entities by hull",
+			description = "Toggle whether finished entities will be highlighted by hull",
+			section = entityHighlightSettings,
+			position = 18
+	)
+	default boolean toggleHighlightFinishedHull()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "highlightFinishedHullColor",
+			name = "Color to highlight finished entities by hull",
+			description = "Which color to highlight finished entities by hull",
+			section = entityHighlightSettings,
+			position = 19
+	)
+	default Color highlightFinishedHullColor()
+	{
+		return new Color(0xFF00FF00);
+	}
+	@ConfigItem(
+			keyName = "highlightFinishedHullInteriorColor",
+			name = "Color to highlight finished entities by hull interior",
+			description = "Which color to highlight finished entities by hull interior",
+			section = entityHighlightSettings,
+			position = 20
+	)
+	default Color highlightFinishedHullInteriorColor()
+	{
+		return new Color(0x1F00FF00);
+	}
+	@ConfigItem(
+			keyName = "toggleHighlightFinishedOutline",
+			name = "Highlight finished entities by outline",
+			description = "Toggle whether finished entities will be highlighted by outline",
+			section = entityHighlightSettings,
+			position = 21
+	)
+	default boolean toggleHighlightFinishedOutline()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "highlightFinishedOutlineColor",
+			name = "Color to highlight finished entities by outline",
+			description = "Which color to highlight finished entities by outline",
+			section = entityHighlightSettings,
+			position = 22
+	)
+	default Color highlightFinishedOutlineColor()
+	{
+		return new Color(0xFF00FF00);
+	}
+	@ConfigItem(
+			keyName = "highlightFinishedOutlineInteriorColor",
+			name = "Color to highlight finished entities by outline interior",
+			description = "Which color to highlight finished entities by outline interior",
+			section = entityHighlightSettings,
+			position = 23
+	)
+	default Color highlightFinishedOutlineInteriorColor()
+	{
+		return new Color(0x1F00FF00);
+	}
+
+	////////////////////////////////////////////////////////////////
+	//////////////////////////////// Entity Scan Settings
+	////////////////////////////////////////////////////////////////
+
+	@ConfigSection(
+			name = "Entity scan settings",
+			description = "Settings that control scans for entities",
+			position = 1,
+			closedByDefault = true
+	)
+	String entityScanSettings = "Entity Scan Settings";
+
+	@Range(
+			max = 30,
+			min = 0
+	)
+	@ConfigItem(
+			keyName = "scanRadius",
+			name = "Scan radius",
+			description = "Distance for entities to be scanned and logged",
+			section = entityScanSettings,
+			position = 0
+	)
+	default int scanRadius()
+	{
+		return 15;
+	}
 
 	@ConfigItem(
 			keyName = "toggleInscopeExamine",
 			name = "Examine in-scope",
 			description = "Toggle whether the examine action is in scope",
-			position = 0
+			section = entityScanSettings,
+			position = 1
 	)
 	default boolean toggleInscopeExamine()
 	{
@@ -34,94 +332,27 @@ public interface EntityLoggerConfig extends Config
 			keyName = "toggleInscopeItemSpawns",
 			name = "Item spawns in-scope",
 			description = "Toggle whether item spawns are in scope",
-			position = 1
+			section = entityScanSettings,
+			position = 2
 	)
 	default boolean toggleInscopeItemSpawns()
 	{
 		return true;
 	}
 
-	@ConfigItem(
-			keyName = "toggleHighlightInscope",
-			name = "Highlight in-scope entities",
-			description = "Toggle whether in-scope entities will be highlighted",
-			position = 2
-	)
-	default boolean toggleHighlightInscope()
-	{
-		return true;
-	}
-	@ConfigItem(
-			keyName = "highlightInscopeColor",
-			name = "In-scope highlight color",
-			description = "Which color to highlight in-scope entities",
-			position = 3
-	)
-	default Color highlightInscopeColor()
-	{
-		return new Color(0xFFFFE533);
-	}
+	////////////////////////////////////////////////////////////////
+	//////////////////////////////// Other Settings
+	////////////////////////////////////////////////////////////////
+
+
 
 	@ConfigItem(
-			keyName = "toggleHighlightBacklogged",
-			name = "Highlight backlogged entities",
-			description = "Toggle whether backlogged entities will be highlighted",
-			position = 4
+		keyName = "greeting",
+		name = "Welcome Greeting",
+		description = "The message to show to the user when they login"
 	)
-	default boolean toggleHighlightBacklogged()
+	default String greeting()
 	{
-		return true;
-	}
-	@ConfigItem(
-			keyName = "highlightBackloggedColor",
-			name = "Backlogged highlight color",
-			description = "Which color to highlight backlogged entities",
-			position = 5
-	)
-	default Color highlightBackloggedColor()
-	{
-		return new Color(0xFFFF0000);
-	}
-
-	@ConfigItem(
-			keyName = "toggleHighlightOutofscope",
-			name = "Highlight out-of-scope entities",
-			description = "Toggle whether out-of-scope entities will be highlighted",
-			position = 6
-	)
-	default boolean toggleHighlightOutofscope()
-	{
-		return true;
-	}
-	@ConfigItem(
-			keyName = "highlightOutofscopeColor",
-			name = "Out-of-scope highlight color",
-			description = "Which color to highlight out-of-scope entities",
-			position = 7
-	)
-	default Color highlightOutofscopeColor()
-	{
-		return new Color(0x7F007F);
-	}
-
-	@ConfigItem(
-			keyName = "toggleHighlightFinished",
-			name = "Highlight finished entities",
-			description = "Toggle whether finished entities will be highlighted",
-			position = 8
-	)
-	default boolean toggleHighlightFinished()
-	{
-		return true;
-	}
-	@ConfigItem(
-			keyName = "highlightFinishedColor",
-			name = "Finished highlight color",
-			description = "Which color to highlight finished entities",
-			position = 9
-	)
-	default Color highlightFinishedColor()
-	{
-		return new Color(0x00FF00);
-	}
+		return "Hello";
+	} //TODO: Get rid of this
 }
